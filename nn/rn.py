5f03a7e4a6528a804806633f2c37357d7bc6b61d
Thales Gonçalves <11820690+thales-goncalves@users.noreply.github.com>
2017-11-16 19:51:08 -0200

    Add files via upload

--- nn/rn.py
+++ nn/rn.py
@@ -9,9 +9,9 @@ from sklearn.neural_network import MLPClassifier
 
 import pandas as pd
 
-#df = pd.read_csv('tae.txt', header=None)
+df = pd.read_csv('tae.txt', header=None)
 #--
-df = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/tae/tae.data', header=None)
+#df = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/tae/tae.data', header=None)
 
 y_train = df.iloc[0:119,[5]].values
 x_train = df.iloc[0:119 , [0,1,2,3,4]].values
@@ -21,7 +21,7 @@ x_test = df.iloc[120:150, [0,1,2,3,4]].values
 
 
 
-mlp = MLPClassifier(hidden_layer_sizes=(30), max_iter=20000, alpha=1e-4,
+mlp = MLPClassifier(hidden_layer_sizes=(30,45), max_iter=20000, alpha=1e-4,
                      solver='sgd', verbose=10, tol=1e-6, random_state=0,
                      learning_rate_init=.0001)
 
@@ -31,3 +31,4 @@ print("Training set score: %f" % mlp.score(x_train, y_train))
 print("Test set score: %f" % mlp.score(x_test, y_test))
 
 
+    
